5f64ae39c4b84c160be847b2700513df46e84134
Santhoshk1983 <santhoshk.1983@gmail.com>
2021-09-12 13:19:16 +0530

    InsertInBetween

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -22,6 +22,15 @@ class linkedlist:
         new_node.next = self.head
         self.head = new_node
 
+    # Inserting after a given node
+    def AddNodeInbetween (self,prev_node,new_data):
+        if prev_node is None:
+            print("Can't insert as there are no previous node. Use HeaderNodeAdd")
+            return
+        new_node = node(new_data)
+        new_node.next=prev_node.next
+        prev_node.next=new_node
+
 ll1 = linkedlist()
 ll1.head = node(1)
 second = node(2)
@@ -30,5 +39,9 @@ third =node(3)
 ll1.head.next=second
 second.next=third
 ll1.printlinkedlist()
+
 ll1.HeaderNodeAdd(0)
+ll1.printlinkedlist()
+
+ll1.AddNodeInbetween(second.next,5)
 ll1.printlinkedlist()

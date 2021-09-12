c705d5fce2d74e0e308c2659eedb03aa8931a856
Santhoshk1983 <santhoshk.1983@gmail.com>
2021-09-12 12:25:45 +0530

    HeaderNodeAddition

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -7,12 +7,20 @@ class linkedlist:
     def __init__(self):
         self.head=None
 
-    def printlinkedlist (self):
+
+    # Traversing linked list
+    def printlinkedlist (self): 
         temp = self.head
 
         while (temp):
             print(temp.data)
             temp = temp.next
+
+    # Inserting as Header Node
+    def HeaderNodeAdd (self, new_data):
+        new_node = node(new_data)
+        new_node.next = self.head
+        self.head = new_node
 
 ll1 = linkedlist()
 ll1.head = node(1)
@@ -21,5 +29,6 @@ third =node(3)
 
 ll1.head.next=second
 second.next=third
-
+ll1.printlinkedlist()
+ll1.HeaderNodeAdd(0)
 ll1.printlinkedlist()
